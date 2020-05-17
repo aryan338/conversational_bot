3b9a5840b0874a9287511f652a4bc83b02bd9668
Aryan Mundada <rnmundra@iitk.ac.in>
2020-05-17 19:14:09 +0530

    aiml

---
+++ Assignments/AIML/app.py
@@ -0,0 +1,19 @@
+import aiml
+import os
+
+kernel = aiml.Kernel()
+if os.path.isfile("bot_brain.brn"):
+    kernel.bootstrap(brainFile = "bot_brain.brn")
+
+else:
+    kernel.bootstrap(learnFiles = "std-startup.xml", commands="load aiml b")
+    kernel.saveBrain("bot_brain.brn")
+
+while True:
+    message = input("Human: ")    
+    if message == "quit":
+        exit()
+    elif message == "save":
+        kernel.saveBrain("bot_brain.brn")
+    else:
+        print("AI: " + kernel.respond(message))
