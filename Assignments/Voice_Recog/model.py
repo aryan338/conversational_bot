f3f46fda04043686c052e4e71b05e8ea53b0f6be
Aryan Mundada <rnmundra@iitk.ac.in>
2020-06-01 20:25:05 +0530

    start of voice

--- Assignments/Voice_Recog/model.py
+++ Assignments/Voice_Recog/model.py
@@ -1,3 +1,44 @@
 import tensorflow as tf
 import numpy as np
+from pydub import AudioSegment
+import random
+import sys
+import io
+import os
+import glob
+import IPython
+from td_utilis import *
+%mathplotlib inline
 
+def get_random_time_segement(segment_ms):
+    segment_start = np.random.randint(low=0, high=10000-segment_ms)
+    segment_end = segment_start + segment_ms - 1
+    
+    return (segment_start, segment_end)
+
+def is_overlapping(segment_time, previous_segments):
+    segment_start, segment_end = segment_time
+    
+    overlap = False
+    
+    for previous_start, previous_end in previous_segments:
+        if segment_start<=previous_end and segment_end>=previous_start:
+            overlap = True
+
+    return overlap
+
+def insert_ones(y, segment_end_ms):
+    
+    segment_end_y = int(segment_end_ms * Ty / 10000.0)
+    
+    for i in range(segment_end_y+1, segment_end_y+51):
+        if i < Ty:
+            y[0, i] = 1
+    
+    return y
+
+from keras.callbacks import ModelCheckpoint
+from keras.models import Model, load_model, Sequential
+from keras.layers import Dense, Activation, Dropout, Input, Masking, TimeDistributed, LSTM, Conv1D
+from keras.layers import GRU, Bidirectional, BatchNormalization, Reshape
+from keras.optimizers import Adam
